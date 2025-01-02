f3b87c203dbbf781e25eb9bdd8e780426955d755
MikaSantos01 <micaias08deus@gmail.com>
2025-01-03 12:48:32 +1400

    Modificações feitas na call, no comando SOBRE

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,13 +9,19 @@
         color: rgb(243, 227, 206);
         text-shadow: 3px 3px 5px #ee2424;
     }
+    .inicio {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
 </style>
 
     <h1 id="titulo">
         {tituloDoJogo}
     </h1>
+    <div class="inicio">
+        <a class="menu" href="/jogar">Jogar</a>
+        <a class="menu" href="/sobre">Sobre</a>
+    </div>
     
-    <br />
-    <a class="menu" href="/jogar">Jogar</a>
-    <br />
-    <a class="menu" href="/sobre">Sobre</a>
